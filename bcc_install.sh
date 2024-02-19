53d0778d12a44f61f425debd49ce127251f87bb1
Topspin <Topspin@node0.deepflow.turbopancake-pg0.utah.cloudlab.us>
2024-02-19 10:53:01 -0700

    bcc_install

---
+++ bcc_install.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/iovisor/bcc.git
+mkdir bcc/build; cd bcc/build
+cmake ..
+make
+sudo make install
+cmake -DPYTHON_CMD=python3 .. # build python3 binding
+pushd src/python/
+make
+sudo make install
+popd
